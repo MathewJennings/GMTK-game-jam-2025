ad145a772e7fdd9d6a603ca1295b1bdc0391c1ec
Mathew Jennings <mathew.percussion@gmail.com>
2025-07-30 22:52:15 -0400

    Make DrawLine.cs clean up old line segments

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,11 +8,14 @@ public class DrawLine : MonoBehaviour
     [SerializeField]
     GameObject linePrefab;
 
+    [SerializeField]
+    float timeToFade;
+
     private GameObject currentLine;
     private LineRenderer lineRenderer;
     private EdgeCollider2D edgeCollider;
-
-    public List<Vector2> drawPositions;
+    private List<Vector2> drawPositions = new(150);
+    private List<float> drawTimes = new(150);
 
     private void Update()
     {
@@ -24,7 +28,14 @@ public class DrawLine : MonoBehaviour
             Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             if (Vector2.Distance(currentPosition, drawPositions[drawPositions.Count - 1]) > .1f)
             {
-                UpdateLine(currentPosition);
+                AddNewPointToLine(currentPosition, Time.time);
+            }
+        }
+        for (int i = 0; i < drawTimes.Count; i++)
+        {
+            if (Time.time - drawTimes[i] > timeToFade)
+            {
+                RemovePointFromLine(i);
             }
         }
     }
@@ -41,12 +52,43 @@ public class DrawLine : MonoBehaviour
         lineRenderer.SetPosition(1, drawPositions[1]);
         edgeCollider.points = drawPositions.ToArray();
     }
-    
-    void UpdateLine(Vector2 newPosition)
+
+    private void AddNewPointToLine(Vector2 currentPosition, float currentTime)
     {
-        drawPositions.Add(newPosition);
-        lineRenderer.positionCount++;
-        lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPosition);
+        if (drawPositions.Count >= 150)
+        {
+            drawPositions.RemoveAt(0);
+            drawTimes.RemoveAt(0);
+        }
+        else
+        {
+            lineRenderer.positionCount++;
+        }
+        drawPositions.Add(currentPosition);
+        drawTimes.Add(currentTime);
+        lineRenderer.SetPositions(vector2ToVector3(drawPositions).ToArray());
         edgeCollider.points = drawPositions.ToArray();
+    }
+
+    private void RemovePointFromLine(int i)
+    {
+        drawPositions.RemoveAt(i);
+        drawTimes.RemoveAt(i);
+        lineRenderer.SetPositions(vector2ToVector3(drawPositions).ToArray());
+        edgeCollider.points = drawPositions.ToArray();
+        if (drawPositions.Count == 2)
+        {
+            GameObject.Destroy(currentLine);
+        }
+    }
+
+    private List<Vector3> vector2ToVector3(List<Vector2> vector2s)
+    {
+        List<Vector3> vector3s = new List<Vector3>(drawPositions.Count);
+        for (int i = 0; i < vector2s.Count; i++)
+        {
+            vector3s.Add(new Vector3(vector2s[i].x, vector2s[i].y, 0f));
+        }
+        return vector3s;
     }
 }

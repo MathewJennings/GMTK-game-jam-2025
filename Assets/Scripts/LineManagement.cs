19aa3555b625ca7f2b92643971d66c7f8a595eee
flee135 <felix.lee135@gmail.com>
2025-07-31 01:23:20 -0400

    Add dynamic loop detection to LineManagement

--- Assets/Scripts/LineManagement.cs
+++ Assets/Scripts/LineManagement.cs
@@ -16,6 +16,7 @@ public class LineManagement : MonoBehaviour
     private bool finishedDrawing = false;
     private List<Vector2> drawPositions;
     private List<float> drawTimes;
+    private List<bool> drawValidForLoops;
 
     /// Cannot be less than 1 second
     public void SetTimeToFade(float time)
@@ -42,14 +43,17 @@ public class LineManagement : MonoBehaviour
 
         drawPositions = new(maxLineLength);
         drawTimes = new(maxLineLength);
+        drawValidForLoops = new(maxLineLength);
 
         drawPositions.Add(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
         lineRenderer.SetPosition(0, drawPositions[0]);
         drawTimes.Add(Time.time);
+        drawValidForLoops.Add(true);
 
         drawPositions.Add(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
         lineRenderer.SetPosition(1, drawPositions[1]);
         drawTimes.Add(Time.time);
+        drawValidForLoops.Add(true);
 
         edgeCollider.points = drawPositions.ToArray();
     }
@@ -98,6 +102,7 @@ public class LineManagement : MonoBehaviour
         {
             drawPositions.RemoveAt(0);
             drawTimes.RemoveAt(0);
+            drawValidForLoops.RemoveAt(0);
         }
         else
         {
@@ -105,14 +110,49 @@ public class LineManagement : MonoBehaviour
         }
         drawPositions.Add(currentPosition);
         drawTimes.Add(currentTime);
+        drawValidForLoops.Add(true);
         lineRenderer.SetPositions(Vector2ListToVector3List(drawPositions).ToArray());
         edgeCollider.points = drawPositions.ToArray();
+        
+        // Check if the newly added point forms a loop.
+        if (CreatedLoop())
+        {
+            Debug.Log("Loop created!");
+            // Mark all the points up until now as invalid.
+            for (int i = 0; i < drawValidForLoops.Count; i++)
+            {
+                drawValidForLoops[i] = false;
+            }
+        }
+    }
+    
+    // Check if the very last point in drawPositions is close to any other point.
+    // Ignore the 15 latest points to avoid false positives.
+    private bool CreatedLoop()
+    {
+        if (drawPositions.Count < 15) return false;
+
+        Vector2 referencePoint = drawPositions[^1];
+        // Start checking at 15 from the end.
+        for (int i = drawPositions.Count - 15; i >= 0; i--)
+        {
+            // Return false if this point was already used in a previous loop.
+            if (!drawValidForLoops[i]) return false;
+            
+            if (Vector2.Distance(referencePoint, drawPositions[i]) < 0.1f)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void RemovePointFromLine(int i)
     {
         drawPositions.RemoveAt(i);
         drawTimes.RemoveAt(i);
+        drawValidForLoops.RemoveAt(i);
         lineRenderer.SetPositions(Vector2ListToVector3List(drawPositions).ToArray());
         edgeCollider.points = drawPositions.ToArray();
         if (drawPositions.Count <= 2)

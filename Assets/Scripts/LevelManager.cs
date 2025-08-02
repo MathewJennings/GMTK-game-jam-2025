769646ab2cb1f4cd0f0000133ae130fb4c1b3812
Mathew Jennings <mathew.percussion@gmail.com>
2025-08-01 20:40:12 -0400

    Create You Win screen that LOOPS back to the tutorial

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,6 +9,7 @@ public class LevelManager : MonoBehaviour
     public SpawnEnemy spawnEnemy; // Assign in Inspector
     public WaveAndBossBarsManager waveAndBossBarsManager; // Assign in Inspector
     public BossProgressBar bossProgressBar; // Assign in Inspector
+    public YouWinUI youWinUI; // Assign in Inspector
 
     void Awake()
     {
@@ -18,7 +19,7 @@ public class LevelManager : MonoBehaviour
         }
         else
         {
-            Debug.LogWarning("LevelManager: Missing reference to SpawnEnemy, StartingLevel, WaveAndBossBarsManager, or BossProgressBar.");
+            Debug.LogWarning("LevelManager: Missing reference to SpawnEnemy, StartingLevel, WaveAndBossBarsManager, BossProgressBar, or YouWinUI.");
         }
     }
 
@@ -34,7 +35,14 @@ public class LevelManager : MonoBehaviour
     private void PrepareNextLevel()
     {
         currentLevel = currentLevel.nextLevel;
-        PrepareCurrentLevel();
+        if (currentLevel != null)
+        {
+            PrepareCurrentLevel();
+        }
+        else
+        {
+            youWinUI.ShowYouWinScreen();
+        }
     }
 
     void Update()
@@ -59,7 +67,7 @@ public class LevelManager : MonoBehaviour
 
     private bool IsFullyConfigured()
     {
-        return spawnEnemy != null && currentLevel != null && waveAndBossBarsManager != null && bossProgressBar != null;
+        return spawnEnemy != null && currentLevel != null && waveAndBossBarsManager != null && bossProgressBar != null && youWinUI != null;
     }
 
     private void PrepareBossFight()
